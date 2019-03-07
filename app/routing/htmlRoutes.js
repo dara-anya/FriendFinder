25e6c83371ee9ad5099842732f7d18049cd9469b
Dara Anya <daraanya@gmail.com>
2019-03-07 09:27:56 -0600

    front-end/initial back-end

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,12 @@
+module.exports = function(server){
+    // Route to home.html
+    app.get("/", function(req, res){
+        res.sendFile(path.join(__dirname, "app/public/home.html"));
+    });
+
+    // Route to survey.html
+    app.get("/survey", function(req, res){
+        res.sendFile(path.join(__dirname, "app/public/survey.html"));
+    });
+}
+
---
+++ server.js
@@ -0,0 +1,43 @@
+// Dependencies
+// ====================================================================
+var express = require("express");
+var path = require("path");
+
+// Set up the Express App
+// ====================================================================
+var app = express();
+
+// Set up the Express app to handle data parsing
+// This helps Express translate the JSON data so it can be requested/posted from the form and pushed into the friends array
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
+var PORT = process.env.PORT || 3000; // This sets the PORT to 3000 if not specified by Heroku
+
+
+
+// Create a friends array to push data to
+friends = [];
+
+// Route URL to display friendsList as JSON 
+app.get('/api/friends', function(req, res){
+    res.json(friends); 
+});
+
+//require("/htmlRoutes")(server);
+// Route to home.html
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+// Route to survey.html
+app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
+// Connect server to PORT
+app.listen(PORT, function(){
+    console.log("app listening on Port: " + PORT);
+});
+
+
